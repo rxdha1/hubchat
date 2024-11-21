032c2335768c817e11ebf0992ac70902c488996d
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:42:46 -0700

    Initialize tech322/tables

---
+++ types/fans.ts
@@ -0,0 +1,25 @@
+import { Album } from "./Album";
+import { Artist } from "./Artist";
+import { AudioBook } from "./AudioBook";
+import { Episode } from "./Episodes";
+import { Playlist } from "./Playlist";
+import { Show } from "./Show";
+import { Track } from "./Track";
+
+export type FAN_TYPE = {
+  name: string | null;
+  country: string | null;
+  city: string | null;
+  product: string | null;
+  playlist: Array<Playlist> | null;
+  episodes: Array<Episode> | null;
+  recentlyPlayed: Array<Track> | null;
+  followedArtists: Array<Artist> | null;
+  savedAlbums: Array<Album> | null;
+  savedAudioBooks: Array<AudioBook> | null;
+  savedShows: Array<Show> | null;
+  savedTracks: Array<Track> | null;
+  topTracks: Array<Track> | null;
+  topArtists: Array<Artist> | null;
+  email: string;
+};
