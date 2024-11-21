d7bcd899732fa7904db48a1cab293fcc51b11851
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:43:25 -0700

    Initialize tech322/top_five_artists

---
+++ app/api/account/route.tsx
@@ -0,0 +1,38 @@
+import { getSupabaseServerAdminClient } from "@/packages/supabase/src/clients/server-admin-client";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const email = body.email;
+  const client = getSupabaseServerAdminClient();
+
+  try {
+    const { data: found } = await client
+      .from("accounts")
+      .select("*")
+      .eq("email", email);
+
+    if (found?.length)
+      return Response.json({ data: found[0] }, { status: 200 });
+
+    const { data: newAccount } = await client
+      .from("accounts")
+      .insert({
+        email,
+        timestamp: Date.now(),
+        artistIds: [],
+      })
+      .select("*")
+      .single();
+
+    return Response.json({ data: newAccount }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    const message = error instanceof Error ? error.message : "failed";
+    return Response.json({ message }, { status: 400 });
+  }
+}
+
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
