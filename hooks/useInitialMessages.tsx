7f8d3d6bd0364ec68f85f73c3d7900a409aff1a3
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:42:32 -0700

    Initialize tech322/habit

---
+++ hooks/useInitialMessages.tsx
@@ -0,0 +1,32 @@
+import { Message } from "ai";
+import { Address } from "viem";
+import { useEffect, useState } from "react";
+import getInitialMessages from "@/lib/stack/getInitialMessages";
+import { usePrivy } from "@privy-io/react-auth";
+import rearrangesMessages from "@/lib/rearrangeMessages";
+
+const useInitialMessages = () => {
+  const [initialMessages, setInitialMessages] = useState<Message[]>([]);
+  const { user } = usePrivy();
+  const address = user?.wallet?.address as Address;
+
+  useEffect(() => {
+    if (address) {
+      fetchInitialMessages(address);
+    }
+  }, [address]);
+
+  const fetchInitialMessages = async (walletAddress: Address) => {
+    try {
+      const messages = await getInitialMessages(walletAddress);
+      const arrangedMessages = rearrangesMessages(messages);
+      setInitialMessages(arrangedMessages);
+    } catch (error) {
+      console.error("Error fetching initial messages:", error);
+    }
+  };
+
+  return { initialMessages };
+};
+
+export default useInitialMessages;
