f4b25bd29eb0c0651367e05c36aa3a17b7076db4
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:43:04 -0700

    Initialize tech322/fix-empty-reply/new-artist

---
+++ hooks/useSuggestions.tsx
@@ -0,0 +1,65 @@
+import { SUGGESTIONS } from "@/lib/consts";
+import trackNewMessage from "@/lib/stack/trackNewMessage";
+import { Message } from "ai";
+import { useEffect, useState } from "react";
+import { Address } from "viem";
+import { v4 as uuidV4 } from "uuid";
+import { useParams, usePathname } from "next/navigation";
+import { useUserProvider } from "@/providers/UserProvder";
+import { useArtistProvider } from "@/providers/ArtistProvider";
+
+const useSuggestions = () => {
+  const { address } = useUserProvider();
+  const { artistActive, selectedArtist } = useArtistProvider();
+  const [suggestions, setSuggestions] = useState(SUGGESTIONS);
+  const [currentQuestion, setCurrentQuestion] = useState<Message | null>(null);
+  const { conversation: pathId } = useParams();
+  const pathname = usePathname();
+  const isNewChat = pathname === "/";
+
+  useEffect(() => {
+    if (artistActive) {
+      setSuggestions([
+        `Who are ${selectedArtist?.name || ""}’s most engaged fans?`,
+        `Analyze ${selectedArtist?.name || ""}’s TikTok posts from this week.`,
+      ]);
+      return;
+    }
+    setSuggestions(SUGGESTIONS);
+  }, [isNewChat, artistActive, selectedArtist]);
+
+  const finalCallback = async (
+    message: Message,
+    lastQuestion?: Message,
+    newConversationId?: string,
+  ) => {
+    const convId = newConversationId || (pathId as string);
+    const question = lastQuestion || currentQuestion;
+    if (!message.content || !question) return;
+    await trackNewMessage(address as Address, question, convId);
+    await trackNewMessage(
+      address as Address,
+      {
+        content: message.content.replace(/[^a-zA-Z0-9\s,._:]/g, ""),
+        role: message.role,
+        id: uuidV4(),
+        questionId: question.id,
+      },
+      convId,
+    );
+    setCurrentQuestion(null);
+    const response = await fetch(`/api/prompts?answer=${message.content}`);
+    const data = await response.json();
+
+    setSuggestions(() => [...data.questions]);
+  };
+
+  return {
+    finalCallback,
+    suggestions,
+    setCurrentQuestion,
+    currentQuestion,
+  };
+};
+
+export default useSuggestions;
