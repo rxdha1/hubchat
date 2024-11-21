e97d521d72074c6c883ac0608ee6e6b8bb630ac8
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:43:29 -0700

    Initialize tech322/responses

---
+++ evals/scripts/campaign_evals_questions_ideals.py
@@ -0,0 +1,65 @@
+from criteria import count_query_criteria, sub_set_criteria
+
+evals_questions_ideals = [
+    {
+        "question": "What is the email count for users with an Apple Music account?",
+        "ideal": "Due to Apple's policy, we do not collect Apple Music emails.",
+        "eval_name": "get_email_count_of_apple",
+        "criteria": "The main points explained in the reference answer and the actual answer should be the same."
+    },
+    {
+        "question": "What is the total number of fans with a premium Spotify account?",
+        "ideal": "premium_fans_count",
+        "eval_name": "get_premium_fans",
+        "criteria": count_query_criteria
+    },
+    {
+        "question": "How many fans have a free Spotify account??",
+        "ideal": "free_fans_count",
+        "eval_name": "get_free_fans",
+        "criteria": count_query_criteria
+    },
+    {
+        "question": "How many unique fans have participated in this campaign?",
+        "ideal": "total_unique_fans_count",
+        "eval_name": "get_total_unique_fans",
+        "criteria": count_query_criteria
+    },
+    {
+        "question": "What are the top five artists that fans are listening to on aggregate?",
+        "ideal": "artists",
+        "eval_name": "get_top_five_artist",
+        "criteria": sub_set_criteria
+    },
+    {
+        "question": "What are the top five playlists among users?",
+        "ideal": "playlists",
+        "eval_name": "get_top_five_playlists",
+        "criteria": sub_set_criteria
+    },
+    {
+        "question": "How many total playlists are fans engaging with?",
+        "ideal": "playlists_count",
+        "eval_name": "get_playlists_count",
+        "criteria": count_query_criteria
+    },
+    {
+        "question": "What are the top three genres that fans listen to outside of hip hop?",
+        "ideal": "genres",
+        "eval_name": "get_top_genres",
+        "criteria": sub_set_criteria
+       
+    },
+    {
+        "question": "What is the country distribution of fans?",
+        "ideal": "format: [country_name]: [fans_count], example: US: 123",
+        "eval_name": "get_country_distribution",
+        "criteria": 'The actual answer should be formatted in the ideal value format.'
+    },
+    {
+        "question": "How many fans logged in with Spotify to participate in this campaign?",
+        "ideal": "spotify_fans_count",
+        "eval_name": "get_spotify_fans_count",
+        "criteria": count_query_criteria
+    }
+]
---
+++ lib/tools/toolSystemMessage.tsx
@@ -0,0 +1,25 @@
+import instructions from "@/evals/scripts/instructions.json";
+
+import { HTML_RESPONSE_FORMAT_INSTRUCTIONS } from "../consts";
+
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
+const toolSystemMessage = (context: any, question: any, toolName: string) => {
+  if (toolName === "getArtistAnalysis")
+    return `
+    Context: ${JSON.stringify(context)}
+    Question: ${question}
+
+    Reply should be 4 sentences or less with actionable insights that focus on creating new campaigns, such as how many TikTok followers you have, how many likes you have, your music style, how you connect with your audience, especially when it comes to your latest single, and what games you can play while listening to music.
+    `;
+  if (toolName === "getCampaign")
+    return `
+    Context: ${JSON.stringify(context)}
+    Question: ${question}
+    ${instructions.get_campaign}
+    In addition, ${instructions.additional_instruction}
+    ${HTML_RESPONSE_FORMAT_INSTRUCTIONS}`;
+
+  return "";
+};
+
+export default toolSystemMessage;
