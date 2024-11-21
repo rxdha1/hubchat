bbbfb6a7fdb527f27a0e0b3fd32da77e91d5e602
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:43:17 -0700

    Initialize tech322/mobile-chip

---
+++ components/Chat/Chat.tsx
@@ -0,0 +1,18 @@
+import { useChatProvider } from "@/providers/ChatProvider";
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
+import { ScrollTo } from "react-scroll-to";
+
+const Chat = () => {
+  const { messages } = useChatProvider();
+  return (
+    <div
+      className={`grow h-[calc(100vh-56px)] md:h-screen overflow-hidden flex flex-col ${messages.length ? "px-4 pb-5 pt-20" : "items-center justify-center"}`}
+    >
+      <ScrollTo>{({ scroll }) => <Messages scroll={scroll} />}</ScrollTo>
+      <ChatInput />
+    </div>
+  );
+};
+
+export default Chat;
