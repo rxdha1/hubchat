66a2640832a5ff66ebc5c6bd039649e8c7bfe262
ZkRYDA <84305422+rxdha1@users.noreply.github.com>
2024-11-21 00:43:06 -0700

    Initialize tech322/new-artist-popup

---
+++ components/Tools/ToolContent.tsx
@@ -0,0 +1,23 @@
+import FanTable from "./FanTable";
+import Artist from "./Artist";
+import { useToolCallProvider } from "@/providers/ToolCallProvider";
+import Campaign from "./Campaign";
+
+const ToolContent = () => {
+  const { toolName } = useToolCallProvider();
+
+  return (
+    <div>
+      {toolName === "getCampaign" && <FanTable />}
+      {(toolName === "createArtist" ||
+        toolName === "getArtists" ||
+        toolName === "getArtistAnalysis" ||
+        toolName === "updateArtistInfo") && <Artist />}
+      {(toolName === "createCampaign" || toolName === "getCampaigns") && (
+        <Campaign />
+      )}
+    </div>
+  );
+};
+
+export default ToolContent;
